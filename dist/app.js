1e5b9405bd3010cd8e06d8e9eb9b9ab2606cbb9b
Camilo Paez <cepn26@gmail.com>
2021-01-23 14:07:54 -0500

    Completas las funciones kick y ban

--- dist/app.js
+++ dist/app.js
@@ -16,10 +16,45 @@ const client = new discord_js_1.Client();
 const pong = (msg) => {
     msg.reply('pong');
 };
+const ban = (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b;
+    if ((_a = msg.member) === null || _a === void 0 ? void 0 : _a.hasPermission('BAN_MEMBERS')) {
+        const member = (_b = msg.mentions.members) === null || _b === void 0 ? void 0 : _b.first();
+        if (member) {
+            try {
+                const kickedMember = yield member.ban();
+                msg.reply(`${kickedMember.nickname} fue baneado del server`);
+            }
+            catch (error) {
+                console.log(error);
+                msg.reply('Error inesperado');
+            }
+        }
+        else {
+            msg.reply('No has mencionado a nadie');
+        }
+    }
+    else {
+        msg.reply('No cuentas con los permisos para realizar el Ban');
+    }
+});
 const kick = (msg) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    if ((_a = msg.member) === null || _a === void 0 ? void 0 : _a.hasPermission('KICK_MEMBERS')) {
-        msg.reply('Tienes permisos');
+    var _c, _d;
+    if ((_c = msg.member) === null || _c === void 0 ? void 0 : _c.hasPermission('KICK_MEMBERS')) {
+        const member = (_d = msg.mentions.members) === null || _d === void 0 ? void 0 : _d.first();
+        if (member) {
+            try {
+                const kickedMember = yield member.kick();
+                msg.reply(`${kickedMember.nickname} fue echado del server`);
+            }
+            catch (error) {
+                console.log(error);
+                msg.reply('Error inesperado');
+            }
+        }
+        else {
+            msg.reply('No has mencionado a nadie');
+        }
     }
     else {
         msg.reply('No cuentas con los permisos para realizar el Kick');
@@ -29,11 +64,19 @@ client.on('ready', () => {
     console.log('Estoy Listo');
 });
 client.on('message', (msg) => {
-    if (msg.content.startsWith('!ping')) {
-        pong(msg);
-    }
-    else if (msg.content.startsWith('!kick')) {
-        kick(msg);
+    if (msg.content.startsWith('!')) {
+        if (msg.content.startsWith('!ping')) {
+            pong(msg);
+        }
+        else if (msg.content.startsWith('!kick')) {
+            kick(msg);
+        }
+        else if (msg.content.startsWith('!ban')) {
+            ban(msg);
+        }
+        else {
+            msg.reply(`El comando ${msg.content} no fue encontrado`);
+        }
     }
 });
 client.login(process.env.TOKEN);
--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,39 @@ const pong = (msg: Message) => {
   msg.reply('pong');
 };
 
+const ban = async (msg: Message) => {
+  if (msg.member?.hasPermission('BAN_MEMBERS')) {
+    const member = msg.mentions.members?.first();
+    if (member) {
+      try {
+        const kickedMember = await member.ban();
+        msg.reply(`${kickedMember.nickname} fue baneado del server`);
+      } catch (error) {
+        console.log(error);
+        msg.reply('Error inesperado');
+      }
+    } else {
+      msg.reply('No has mencionado a nadie');
+    }
+  } else {
+    msg.reply('No cuentas con los permisos para realizar el Ban');
+  }
+};
+
 const kick = async (msg: Message) => {
   if (msg.member?.hasPermission('KICK_MEMBERS')) {
-    msg.reply('Tienes permisos');
+    const member = msg.mentions.members?.first();
+    if (member) {
+      try {
+        const kickedMember = await member.kick();
+        msg.reply(`${kickedMember.nickname} fue echado del server`);
+      } catch (error) {
+        console.log(error);
+        msg.reply('Error inesperado');
+      }
+    } else {
+      msg.reply('No has mencionado a nadie');
+    }
   } else {
     msg.reply('No cuentas con los permisos para realizar el Kick');
   }
@@ -21,10 +51,16 @@ client.on('ready', () => {
 });
 
 client.on('message', (msg: Message) => {
-  if (msg.content.startsWith('!ping')) {
-    pong(msg);
-  } else if (msg.content.startsWith('!kick')) {
-    kick(msg);
+  if (msg.content.startsWith('!')) {
+    if (msg.content.startsWith('!ping')) {
+      pong(msg);
+    } else if (msg.content.startsWith('!kick')) {
+      kick(msg);
+    } else if (msg.content.startsWith('!ban')) {
+      ban(msg);
+    } else {
+      msg.reply(`El comando ${msg.content} no fue encontrado`);
+    }
   }
 });
 
